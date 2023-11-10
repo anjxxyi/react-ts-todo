b1e8d3e5e0cfdcc6e13de73494ac68a620198e4a
anjxxyi <anjxxyi@gmail.com>
2023-11-10 13:54:25 +0900

    feat: 할일 완료 기능 구현
    - 완료 버튼 클릭시, completed 값 변경(boolean)
    - 완료할 경우 해당 할일 내용 취소선 style

--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -30,13 +30,25 @@ export default function TodoItem({ id, content, completed, onClickDelete, onClic
     setIsEdited(false);
   }
 
+  const completeHandler = () => {
+    const updatedTodo = {
+      id: id,
+      content: content,
+      completed: !completed,
+    };
+
+    onClickUpdate(updatedTodo);
+  };
+
 
   return (
     <>
     { !isEdited ? (
       <li className="todoItem">
-        <input type="checkbox" name="" id="" checked={completed}/>
-        <p>{content}</p>
+        <button type="button" onClick={completeHandler}>
+          { completed ? "✔" : null }
+        </button>
+        <p style={ completed ? { textDecoration: "line-through" } : undefined }>{content}</p>
         <div className="btnBox">
           <button type="button" onClick={() => setIsEdited(true)}>수정</button>
           <button type="button" onClick={() => onClickDelete(id)}>삭제</button>
